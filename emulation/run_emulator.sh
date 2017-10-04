e969e0ae71529c6184b21c3651cfb6d23fed977e
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2017-10-04 16:05:39 +0300

    Fixed gitignores

--- emulation/run_emulator.sh
+++ emulation/run_emulator.sh
@@ -1,5 +1,6 @@
 set -e
 cp ../android/app/src/main/java/com/verbozecontrol/socket/CommunicationManager.java com/verbozecontrol/socket/
+mkdir -p build/
 javac -Xlint:unchecked -d build/ -cp ".:lib/json-simple-1.1.1.jar" com/verbozecontrol/MainApplication.java
 cp lib/json-simple-1.1.1.jar build/
 cd build
