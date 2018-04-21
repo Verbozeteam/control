e55d7d3549f1e514aae2181d3acbea6169b0e0f5
Mohammed M. Fituri <mfituri94@gmail.com>
2018-04-21 15:41:23 +0300

    updated Grand Hyatt bathroom tab photo

--- components/PagingView.js
+++ components/PagingView.js
@@ -63,7 +63,7 @@ class PagingView extends React.Component<any, StateType> {
 
     _backgrounds = {
         'dimmers': require('../assets/images/room-lights.png'),
-        'light_switches': require('../assets/images/bathroom-lights.png'),
+        'light_switches': require('../assets/images/bathroom-lights2.png'),
         'curtains': require('../assets/images/curtain_back.jpg'),
         'hotel_controls': require('../assets/images/services_stack.jpg'),
         'central_acs': require('../assets/images/thermostat_stack.jpg'),
