7a6e3980d41e832092d6fddbc1f710f844b16461
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2018-04-26 17:35:26 +0300

    fixed small bug in deployment parameters file

--- deployment.js
+++ deployment.js
@@ -6,18 +6,19 @@ var default_ip = "{{TARGET_IP}}";
 var default_port = "{{TARGET_PORT}}";
 var default_ssid = "{{SSID}}";
 var default_passphrase = "{{PASSWORD}}";
+
+if (default_ssid === ("{{SS" + "ID}}")) default_ssid = "";
+if (default_passphrase === ("{{PASS" + "WORD}}")) default_passphrase = "";
+if (default_ip === ("{{TARGE" + "T_IP}}")) default_ip = "192.168.10.1";
+if (default_port === ("{{TARGET" + "_PORT}}")) default_port = 7990;
+else if (typeof(default_port) === "string") default_port = parseInt(default_port);
+
 try {
     if (__DEV__) {
         default_ip = "10.11.28.190";
         default_port = 4567;
     }
 } catch (e) {}
-
-if (default_ssid === "{{SSID}}") default_ssid = "";
-if (default_passphrase === "{{PASSWORD}}") default_passphrase = "";
-if (default_ip === "{{TARGET_IP}}") default_ip = "192.168.10.1";
-if (default_port === "{{TARGET_PORT}}") default_port = 7990;
-else if (typeof(default_port) === "string") default_port = parseInt(default_port);
 
 export {
     defaultUIStyle,
