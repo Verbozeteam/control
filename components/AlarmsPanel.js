35d0a978f9cbfb1ea35766140dec70d97c3ee276
Mohammed M. Fituri <mfituri94@gmail.com>
2018-05-02 16:47:38 +0300

    added Alarms panel

---
+++ components/AlarmsPanel.js
@@ -0,0 +1,26 @@
+/* @flow */
+
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// import AnalogClock from './AnalogClock';
+
+type PropsType = {};
+type StateType = {};
+
+export default class AlarmsPanel extends React.Component<PropsType> {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <AnalogClock /> */}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
